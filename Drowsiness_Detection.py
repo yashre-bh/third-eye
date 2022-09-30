e3175e2a260f8d1e29aecb0a41082a18585705d2
LunaticFrisbee <avnishchauhan065@gmail.com>
2022-10-01 03:45:57 +0530

    fix logical errors

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -3,6 +3,9 @@ from imutils import face_utils
 import imutils
 import dlib
 import cv2
+import numpy as np
+from PIL import ImageGrab
+
 
 def eye_aspect_ratio(eye):
 	A = distance.euclidean(eye[1], eye[5])
@@ -10,31 +13,35 @@ def eye_aspect_ratio(eye):
 	C = distance.euclidean(eye[0], eye[3])
 	ear = (A + B) / (2.0 * C)
 	return ear
-	
+
+
 thresh = 0.25
 frame_check = 20
 detect = dlib.get_frontal_face_detector()
-predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
+# Dat file is the crux of the code
+predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
-cap=cv2.VideoCapture(0)
-flag=0
+# cap = cv2.VideoCapture(0)
+flag = 0
 print(1)
 while True:
-	ret, frame=cap.read()
-	frame = imutils.resize(frame, width=450)
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	subjects = detect(gray, 0)
-	if not subjects :
+    img = ImageGrab.grab(bbox=(0, 0, 1000, 1000))  # x,y,w,h
+	# ret, frame = cap.read()
+	# frame = imutils.resize(frame, width=450);
+    img = imutils.resize(img, width=450)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    subjects = detect(gray, 0)
+    if not subjects:
 		flag += 1
-		print (flag)
+		print(flag)
 		if flag >= frame_check:
-			cv2.putText(frame, "****************ALERT!****************", (10, 30),
+			cv2.putText(img, "****************ALERT!****************", (10, 30),
 				cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-			cv2.putText(frame, "****************ALERT!****************", (10,325),
+			cv2.putText(img, "****************ALERT!****************", (10, 325),
 				cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-	
+
 	for subject in subjects:
 		shape = predict(gray, subject)
 		shape = face_utils.shape_to_np(shape)#converting to NumPy Array
@@ -45,23 +52,24 @@ while True:
 		ear = (leftEAR + rightEAR) / 2.0
 		leftEyeHull = cv2.convexHull(leftEye)
 		rightEyeHull = cv2.convexHull(rightEye)
-		cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
-		cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(img, [leftEyeHull], -1, (0, 255, 0), 1)
+		cv2.drawContours(img, [rightEyeHull], -1, (0, 255, 0), 1)
 		if ear < thresh:
 			flag += 1
 			print (flag)
 			if flag >= frame_check:
-				cv2.putText(frame, "****************ALERT!****************", (10, 30),
+				cv2.putText(img, "****************ALERT!****************", (10, 30),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				cv2.putText(frame, "****************ALERT!****************", (10,325),
+				cv2.putText(img, "****************ALERT!****************", (10,325),
 					cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-				#print ("Drowsy")
+				# print ("Drowsy")
 		else:
 			flag = 0
 	
-	cv2.imshow("Frame", frame)
+	cv2.imshow("Frame", img)
 	key = cv2.waitKey(1) & 0xFF
 	if key == ord("q"):
 		break
+
 cv2.destroyAllWindows()
-cap.release() 
+# cap.release() 
