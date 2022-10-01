949e9db21c66ca363e816415b01bd7a028123a1b
Parth-Nebula <parthmittal.nebula@gmail.com>
2022-10-01 12:56:31 +0530

    clean code

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -7,7 +7,6 @@ import numpy as np
 import mss
 
 
-
 def eye_aspect_ratio(eye):
 	A = distance.euclidean(eye[1], eye[5])
 	B = distance.euclidean(eye[2], eye[4])
@@ -18,33 +17,39 @@ def eye_aspect_ratio(eye):
 
 thresh = 0.25
 frame_check = 20
+
 detect = dlib.get_frontal_face_detector()
 # Dat file is the crux of the code
 predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 
 (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
 (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
-# cap = cv2.VideoCapture(0)
-flag = 0
-print(1)
+
+print("Model Initialization Complete")
+
+
+
 
 monitor = {"top": 0, "left": 0, "width": 1366, "height": 768}
 
-def horizontal_checker ( i ) :
+
+
+
+def horizontalChecker ( i ) :
 
     for j in range ( monitor["width"] ) :
 
-        if [ img_short[i][j][0] , img_short[i][j][1] , img_short[i][j][2] , img_short[i][j][3] ] != [ 36 , 33 , 32 , 255 ] :
+        if [ img_initial_nump[i][j][0] , img_initial_nump[i][j][1] , img_initial_nump[i][j][2] , img_initial_nump[i][j][3] ] != [ 36 , 33 , 32 , 255 ] :
 
             return 0
 
     return 1
 
-def vertical_checker ( j , i1 , i2 ) :
+def verticalChecker ( j , i1 , i2 ) :
 
     for i in range ( i1 , i2+1 ) :
 
-        if [ img_short[i][j][0] , img_short[i][j][1] , img_short[i][j][2] , img_short[i][j][3] ] != [ 36 , 33 , 32 , 255 ] :
+        if [ img_initial_nump[i][j][0] , img_initial_nump[i][j][1] , img_initial_nump[i][j][2] , img_initial_nump[i][j][3] ] != [ 36 , 33 , 32 , 255 ] :
 
             return 0
 
@@ -55,24 +60,17 @@ def vertical_checker ( j , i1 , i2 ) :
 
 for i in range(20) :
     
-    img_np = np.array(mss.mss().grab(monitor))
+    img_initial_nump = np.array(mss.mss().grab(monitor))
 
-    img_short = img_np
+print("Initial Screen Shot Taken")
 
-    #img_short = imutils.resize ( img_np , width = 450 )
-    
-    cv2.imshow("OpenCV/Numpy normal", img_short )
+horizontal_cuts = []
 
+black_row = [0] * len(img_initial_nump)
 
-print("sss done")
+for i in range ( len(img_initial_nump) ) :
 
-Vals = []
-
-black_row = [0] * len(img_short)
-
-for i in range ( len(img_short) ) :
-
-    if not horizontal_checker ( i ) :
+    if not horizontalChecker ( i ) :
 
         continue
 
@@ -82,21 +80,21 @@ for i in range ( len(img_short) ) :
 
         continue
 
-    Vals += [ i ]
+    horizontal_cuts += [ i ]
 
-Vals = Vals[:-1]
+horizontal_cuts = horizontal_cuts[:-1]
 
-Vals22 = []
+all_vertical_cuts = []
 
-for i in range ( len(Vals) - 1 ) :
+for i in range ( len(horizontal_cuts) - 1 ) :
 
-    Vals2 = []
+    vertical_cuts = []
 
     black_column = [0] * monitor["width"]
 
     for j in range ( monitor["width"] ) :
 
-        if not vertical_checker ( j , Vals[i] , Vals[i+1] ) :
+        if not verticalChecker ( j , horizontal_cuts[i] , horizontal_cuts[i+1] ) :
 
             continue
 
@@ -106,64 +104,59 @@ for i in range ( len(Vals) - 1 ) :
 
             continue
 
-        Vals2 += [ j ]
+        vertical_cuts += [ j ]
 
-    Vals22 += [Vals2]
-
-
-boxes = 0
-boxinrow = []
-for i in Vals22 :
-
-    boxes += len(i)-1
-
-    boxinrow += [ len(i) - 1 ]
-
-flag = [0] * boxes
-
-penalty = [0] * boxes
+    all_vertical_cuts += [vertical_cuts]
 
 
-boxbefrow = [0]
+no_of_boxes = 0
+boxes_in_a_row = []
+for i in all_vertical_cuts :
 
-for i in range( len(boxinrow) - 1 ) :
+    no_of_boxes += len(i)-1
 
-        boxbefrow += [ boxbefrow[-1] + boxinrow[i] ]
+    boxes_in_a_row += [ len(i) - 1 ]
+
+flag = [0] * no_of_boxes
+
+penalty = [0] * no_of_boxes
 
 
-print("ppp done")
+boxes_before_a_row = [0]
+
+for i in range( len(boxes_in_a_row) - 1 ) :
+
+        boxes_before_a_row += [ boxes_before_a_row[-1] + boxes_in_a_row[i] ]
+
+
+print("Preprocessing Done")
 
 
 while True:
-    img_size= {"top": 0, "left": 0, "width": 1366, "height": 768}
-    img_np_full = np.array(mss.mss().grab(img_size))
-
-    # ret, frame = cap.read()
-    # frame = imutils.resize(frame, width=450);
     
-    #img_np_full = imutils.resize(img_np_one, width=450)
-
-    
+    img_nump = np.array(mss.mss().grab(monitor))
 
 
-    for height_i in range ( len(Vals) - 1 ) :
+    for row_no in range ( len(horizontal_cuts) - 1 ) :
 
-        for position_j in range ( len(Vals22[height_i]) - 1 ) :
-
+        for column_no in range ( len(all_vertical_cuts[row_no]) - 1 ) :
             
-            img_np = [ img_np_full[iii][Vals22[height_i][position_j] : Vals22[height_i][position_j + 1]+1] for iii in range ( Vals[height_i] , Vals[height_i +1] + 1 ) ]
-
-            img_np = np.array(img_np)
-
-            img_np = imutils.resize(img_np, width=450)
             
-            gray = cv2.cvtColor(img_np, cv2.COLOR_BGR2GRAY)
+            user_no = boxes_before_a_row[row_no] + column_no
+            
+            user_img_nump = [ img_nump[iii][all_vertical_cuts[row_no][column_no] : all_vertical_cuts[row_no][column_no + 1]+1] for iii in range ( horizontal_cuts[row_no] , horizontal_cuts[row_no +1] + 1 ) ]
+
+            user_img_nump = np.array(user_img_nump)
+
+            user_img_nump = imutils.resize(user_img_nump, width=450)
+            
+            gray = cv2.cvtColor(user_img_nump, cv2.COLOR_BGR2GRAY)
             subjects = detect(gray, 0)
             if not subjects: 
-                flag[boxbefrow[height_i] + position_j] += 1
-                print(flag)
-                if flag[boxbefrow[height_i] + position_j] >= frame_check:
-                    penalty[boxbefrow[height_i] + position_j] +=1
+                flag[user_no] += 1
+                #print(flag)
+                if flag[user_no] >= frame_check:
+                    penalty[user_no] +=1
             for subject in subjects:
 
                 shape = predict(gray, subject)
@@ -175,19 +168,19 @@ while True:
                 ear = (leftEAR + rightEAR) / 2.0
                 leftEyeHull = cv2.convexHull(leftEye)
                 rightEyeHull = cv2.convexHull(rightEye)
-                cv2.drawContours(img_np, [leftEyeHull], -1, (0, 255, 0), 1)
-                cv2.drawContours(img_np, [rightEyeHull], -1, (0, 255, 0), 1)
+                cv2.drawContours(user_img_nump, [leftEyeHull], -1, (0, 255, 0), 1)
+                cv2.drawContours(user_img_nump, [rightEyeHull], -1, (0, 255, 0), 1)
                 if ear < thresh:
-                    flag[boxbefrow[height_i] + position_j] += 1
-                    print(flag)
-                    if flag[boxbefrow[height_i] + position_j] >= frame_check:
-                        penalty[boxbefrow[height_i] + position_j] +=1
+                    flag[user_no] += 1
+                    #print(flag)
+                    if flag[user_no] >= frame_check:
+                        penalty[user_no] +=1
                 else:
-                    flag[boxbefrow[height_i] + position_j] = 0
+                    flag[user_no] = 0
             
-            if boxbefrow[height_i] + position_j == 2 :
+            if user_no == 2 :
 
-                cv2.imshow("Frame", img_np)
+                cv2.imshow("Frame", user_img_nump)
 
                 
     key = cv2.waitKey(1) & 0xFF
@@ -195,4 +188,3 @@ while True:
         break
 
 cv2.destroyAllWindows()
-# cap.release() 
