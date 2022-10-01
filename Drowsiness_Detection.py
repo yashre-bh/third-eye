11c9a9e36f6d33ccba664c2eeb13652c2c7af094
unknown <pradnyashimpi22@gmail.com>
2022-10-01 05:30:06 +0530

    solve error of eye detection

--- Drowsiness_Detection.py
+++ Drowsiness_Detection.py
@@ -4,7 +4,9 @@ import imutils
 import dlib
 import cv2
 import numpy as np
+import mss
 from PIL import ImageGrab
+
 
 
 
@@ -28,8 +30,9 @@ predict = dlib.shape_predictor("models/shape_predictor_68_face_landmarks.dat")
 flag = 0
 print(1)
 while True:
-    img = ImageGrab.grab(bbox=(0, 0, 1000, 1000))  # x,y,w,h
-    img_np = np.array(img)
+    img_size= {"top": 40, "left": 0, "width": 800, "height": 640}
+    img_np = np.array(mss.mss().grab(img_size))
+
     # ret, frame = cap.read()
     # frame = imutils.resize(frame, width=450);
     img_np = imutils.resize(img_np, width=450)
@@ -40,38 +43,38 @@ while True:
         print(flag)
         if flag >= frame_check:
             cv2.putText(img_np, "****************ALERT!****************", (10, 30),
-	    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+	    	cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
             cv2.putText(img_np, "****************ALERT!****************", (10, 325),
             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-        for subject in subjects:
+    for subject in subjects:
 
-            shape = predict(gray, subject)
-            shape = face_utils.shape_to_np(shape)#converting to NumPy Array
-            leftEye = shape[lStart:lEnd]
-            rightEye = shape[rStart:rEnd]
-            leftEAR = eye_aspect_ratio(leftEye)
-            rightEAR = eye_aspect_ratio(rightEye)
-            ear = (leftEAR + rightEAR) / 2.0
-            leftEyeHull = cv2.convexHull(leftEye)
-            rightEyeHull = cv2.convexHull(rightEye)
-            cv2.drawContours(img_np, [leftEyeHull], -1, (0, 255, 0), 1)
-            cv2.drawContours(img_np, [rightEyeHull], -1, (0, 255, 0), 1)
-            if ear < thresh:
-                flag += 1
-                print (flag)
-                if flag >= frame_check:
-                    cv2.putText(img_np, "****************ALERT!****************", (10, 30),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
-                    cv2.putText(img_np, "****************ALERT!****************", (10,325),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+        shape = predict(gray, subject)
+        shape = face_utils.shape_to_np(shape)#converting to NumPy Array
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEAR = eye_aspect_ratio(leftEye)
+        rightEAR = eye_aspect_ratio(rightEye)
+        ear = (leftEAR + rightEAR) / 2.0
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(img_np, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(img_np, [rightEyeHull], -1, (0, 255, 0), 1)
+        if ear < thresh:
+            flag += 1
+            print (flag)
+            if flag >= frame_check:
+                cv2.putText(img_np, "****************ALERT!****************", (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                cv2.putText(img_np, "****************ALERT!****************", (10,325),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
 				# print ("Drowsy")
-                else:
-                    flag = 0
+            else:
+                flag = 0
 	
-        cv2.imshow("Frame", img_np)
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord("q"):
-            break
+    cv2.imshow("Frame", img_np)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
 
 cv2.destroyAllWindows()
 # cap.release() 
