0cff6c419513b3bf851477c209d6c3e186a348fc
Shreya Bhagat <shreyabhagat2002@gmail.com>
2022-10-02 17:18:46 +0530

    fix flow

--- server.py
+++ server.py
@@ -2,16 +2,14 @@ import os
 from flask import Flask, render_template
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/my-link/')
+@app.route('/results')
 def my_link():
-  print ('I got clicked!')
-  os.system("Drowsiness_Detection.py")
+  print(os.getcwd())
+  os.system("python3 Drowsiness_Detection.py")
   return render_template('analysis.html')
 
 if __name__ == '__main__':
